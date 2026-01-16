42cd0fc4a3093dbc273b7f280adb34fe565e07a9
Loquitox <109006108+Loquitox@users.noreply.github.com>
2026-01-16 16:56:08 -0300

    script.js

--- script.js
+++ script.js
@@ -1,33 +1,17 @@
 const textoInput = document.getElementById('textoInput');
 const textoPreview = document.getElementById('textoPreview');
 const textoCircularEl = document.getElementById('textoCircularEl');
-const invertirBtn = document.getElementById('invertirBtn');
 
-let invertido = false;
+let rotacion = 0;
+let arrastrando = false;
+let inicioX = 0;
 
 // texto en tiempo real
 textoInput.addEventListener('input', () => {
   textoPreview.textContent = textoInput.value || 'TU TEXTO ACÁ';
 });
 
-// dirección inversa (path normal / invertido)
-invertirBtn.addEventListener('click', () => {
-  textoPreview.setAttribute(
-    'href',
-    invertido ? '#pathNormal' : '#pathInvertido'
-  );
-  invertido = !invertido;
-});
-
-// --- DRAG ---
-let arrastrando = false;
-let inicioX = 0;
-let offsetActual = 50;
-
-// centrado inicial
-textoPreview.setAttribute('startOffset', offsetActual + '%');
-
-// mouse down sobre el TEXTO
+// drag 360 real
 textoCircularEl.addEventListener('mousedown', (e) => {
   arrastrando = true;
   inicioX = e.clientX;
@@ -35,24 +19,20 @@ textoCircularEl.addEventListener('mousedown', (e) => {
   e.preventDefault();
 });
 
-// mouse move
 document.addEventListener('mousemove', (e) => {
   if (!arrastrando) return;
 
   const delta = e.clientX - inicioX;
-  offsetActual += delta * 0.05;
+  rotacion += delta * 0.4; // sensibilidad (ajustable)
 
-const margen = 15; // zona segura
+  textoCircularEl.setAttribute(
+    'transform',
+    `rotate(${rotacion} 210 210)`
+  );
 
-if (offsetActual < margen) offsetActual = margen;
-if (offsetActual > 100 - margen) offsetActual = 100 - margen;
-
-
-  textoPreview.setAttribute('startOffset', offsetActual + '%');
   inicioX = e.clientX;
 });
 
-// mouse up
 document.addEventListener('mouseup', () => {
   arrastrando = false;
   textoCircularEl.style.cursor = 'grab';
